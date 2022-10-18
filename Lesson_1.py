b1143d02089b887d1eb3015483d7d1efe404870f
Autohexy <burikohleb@gmail.com>
2022-10-19 00:24:56 +0300

    Update Lesson_1.py

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,15 +1,23 @@
 a=int(input("Input a number: "))
-b=int(input("Input a second number: "))
-action=int(input("Input an action 1=+; 2=-; 3=/; 4=*; : "))
+action=(input("Input an action +; -; /; *; ^; sqrt; : "))
 
-if action==1:
+if action=="+":
+    b=int(input("Input a second number: "))
     print("Result", str(a+b))
-elif action==2:
+elif action=="-":
+    b=int(input("Input a second number: "))
     print("Result", str(a-b))
-elif action==3:
+elif action=="/":
+    b=int(input("Input a second number: "))
     print("Result", str(a/b))
-elif action==4:
+elif action=="*":
+    b=int(input("Input a second number: "))
     print("Result", str(a*b))
+elif action=="^":
+    print("Result ", str(a**2))
+elif action=="sqrt":
+    print("Result", str(a**0.5))
+
 else:
     print("NaN")
 
