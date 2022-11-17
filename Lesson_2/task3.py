20c40ccea220ff4d01f4c50c0eeeb7ce3d545cce
Autohexy <burikohleb@gmail.com>
2022-11-17 19:55:00 +0200

    Create task3.py

---
+++ Lesson_2/task3.py
@@ -0,0 +1,11 @@
+Number=int(input("Enter a number: "))
+while Number>1:
+    Subtract=Number
+    while Subtract>1:
+        Subtract=Subtract-1
+        print(Subtract)
+    Number=Number-1
+print("Done: ")
+
+    
+    
